9fac2f12fc7105ea1597a1358d1c4619ee2f0819
Angelo Putignano <angelo.putignano.16@gmail.com>
2024-12-20 17:56:53 +0100

    Updated RequestManager

--- Src/API/streamingwatch.py
+++ Src/API/streamingwatch.py
@@ -1,11 +1,14 @@
-from bs4 import BeautifulSoup,SoupStrainer 
+from bs4 import BeautifulSoup, SoupStrainer
 from Src.Utilities.convert import get_TMDb_id_from_IMDb_id
 from Src.Utilities.info import get_info_tmdb, is_movie, get_info_imdb
 import Src.Utilities.config as config
 import re
 import json
 import urllib.parse
+
 SW_DOMAIN = config.SW_DOMAIN
+
+
 async def wponce_get(client):
     response = await client.get(f"https://www.streamingwatch.{SW_DOMAIN}/contatto/")
     pattern = r'"admin_ajax_nonce":"(\w+)"'
@@ -13,7 +16,8 @@ async def wponce_get(client):
     wponce = matches[1]
     return wponce
 
-async def search(showname,season,episode,date,ismovie,client):
+
+async def search(showname, season, episode, date, ismovie, client):
     if ismovie == 1:
         wponce = await wponce_get(client)
         query = f'https://www.streamingwatch.{SW_DOMAIN}/wp-admin/admin-ajax.php'
@@ -40,81 +44,75 @@ async def search(showname,season,episode,date,ismovie,client):
             '_wpnonce': wponce,
         }
         cookies = {
-             'wordpress_test_cookie': 'WP%20Cookie%20check',
+            'wordpress_test_cookie': 'WP%20Cookie%20check',
         }
-        response = await client.post(query,cookies=cookies, headers=headers, data=data)
-        soup = BeautifulSoup(response.content,'lxml')
-        page_date = soup.find(id = 'search-cat-year').text.strip()
+        response = await client.post(query, cookies=cookies, headers=headers, data=data)
+        soup = BeautifulSoup(response.content, 'lxml')
+        page_date = soup.find(id='search-cat-year').text.strip()
         if page_date == date:
-           href = soup.find('a')['href']
-           response = await client.get(href, allow_redirects=True, impersonate = "chrome120")
-           soup = BeautifulSoup(response.text,'lxml',parse_only=SoupStrainer('iframe'))
-           iframe = soup.find('iframe')
-           hdplayer = iframe.get('data-lazy-src')
+            href = soup.find('a')['href']
+            response = await client.get(href, allow_redirects=True, impersonate="chrome120")
+            soup = BeautifulSoup(response.text, 'lxml', parse_only=SoupStrainer('iframe'))
+            iframe = soup.find('iframe')
+            hdplayer = iframe.get('data-lazy-src')
 
-           return hdplayer
+            return hdplayer
     elif ismovie == 0:
         #Some series have the name in english so we first search with the categories option and then we use the obtained ID to get all the episodes
-        id_response = await client.get(f'https://streamingwatch.{SW_DOMAIN}/wp-json/wp/v2/categories?search={showname}&_fields=id', allow_redirects=True, impersonate = "chrome120")
+        id_response = await client.get(
+            f'https://streamingwatch.{SW_DOMAIN}/wp-json/wp/v2/categories?search={showname}&_fields=id',
+            allow_redirects=True, impersonate="chrome120")
         data = json.loads(id_response.text)
         category_id = data[0]['id']
         query = f'https://streamingwatch.{SW_DOMAIN}/wp-json/wp/v2/posts?categories={category_id}&per_page=100'
-        response = await client.get(query, allow_redirects=True, impersonate = "chrome120")
+        response = await client.get(query, allow_redirects=True, impersonate="chrome120")
         data_json = response.text
         data = json.loads(data_json)
         for entry in data:
-            if f"stagione-{season}-episodio-{episode}" in entry["slug"] and f"stagione-{season}-episodio-{episode}0" not in entry["slug"]:
+            if f"stagione-{season}-episodio-{episode}" in entry[
+                "slug"] and f"stagione-{season}-episodio-{episode}0" not in entry["slug"]:
                 content = entry["content"]["rendered"]
                 #"content":{
-#    "rendered":"<p><!--baslik:PRO--><iframe loading=\"lazy\" src=\"https:\/\/hdplayer.gives\/embed\/YErLVq64uNTZRNz\" frameborder=\"0\" width=\"700\" height=\"400\" allowfullscreen><\/iframe><\/p>\n","protected":false}
-                start = content.find('src="') + len('src="') #start of url
-                end = content.find('"', start) #end of url
+                #    "rendered":"<p><!--baslik:PRO--><iframe loading=\"lazy\" src=\"https:\/\/hdplayer.gives\/embed\/YErLVq64uNTZRNz\" frameborder=\"0\" width=\"700\" height=\"400\" allowfullscreen><\/iframe><\/p>\n","protected":false}
+                start = content.find('src="') + len('src="')  #start of url
+                end = content.find('"', start)  #end of url
                 hdplayer = content[start:end]
                 return hdplayer
-async def hls_url(hdplayer,client):
-    response = await client.get(hdplayer, allow_redirects=True, impersonate = "chrome120")
+
+
+async def hls_url(hdplayer, client):
+    response = await client.get(hdplayer, allow_redirects=True, impersonate="chrome120")
     match = re.search(r'sources:\s*\[\s*\{\s*file\s*:\s*"([^"]*)"', response.text)
     url = match.group(1)
     return url
-async def streamingwatch(imdb,client):
-    try:
-       general = is_movie(imdb)
-       ismovie = general[0]
-       imdb_id = general[1]
-       type = "StreamingWatch"
-       if ismovie == 0:
-           season  = int(general[2])
-           episode = int(general[3])
-           if "tt" in imdb:
-               tmdba = await get_TMDb_id_from_IMDb_id(imdb_id,client)
-           else:
-               tmdba = imdb_id
-       else:
-           season = None
-           episode = None
-           if "tt" in imdb:
-               tmdba = await get_TMDb_id_from_IMDb_id(imdb_id,client)
-           else:
-               tmdba = imdb_id
-       showname,date = get_info_tmdb(tmdba,ismovie,type)
-       showname = showname.replace(" ", "+").replace("–", "+").replace("—","+")
-       hdplayer = await search(showname,season,episode,date,ismovie,client)
-       url = await hls_url(hdplayer,client)
-       print(f"MammaMia: StreamingWatch found results for {showname}")
-       return url
-    except Exception as e:
-          print("MammaMia: StreamingWatch Failed",e)
-          return None
-    
-'''
-async def test_animeworld():
-    from curl_cffi.requests import AsyncSession
-    async with AsyncSession() as client:
-        # Replace with actual id, for example 'anime_id:episode' format
-        test_id = "tt16426418"  # This is an example ID format
-        results = await streamingwatch(test_id, client)
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(test_animeworld())
-'''
+
+async def streamingwatch(imdb, client):
+    try:
+        general = is_movie(imdb)
+        ismovie = general[0]
+        imdb_id = general[1]
+        type = "StreamingWatch"
+        if ismovie == 0:
+            season = int(general[2])
+            episode = int(general[3])
+            if "tt" in imdb:
+                tmdba = await get_TMDb_id_from_IMDb_id(imdb_id, client)
+            else:
+                tmdba = imdb_id
+        else:
+            season = None
+            episode = None
+            if "tt" in imdb:
+                tmdba = await get_TMDb_id_from_IMDb_id(imdb_id, client)
+            else:
+                tmdba = imdb_id
+        showname, date = get_info_tmdb(tmdba, ismovie, type)
+        showname = showname.replace(" ", "+").replace("–", "+").replace("—", "+")
+        hdplayer = await search(showname, season, episode, date, ismovie, client)
+        url = await hls_url(hdplayer, client)
+        print(f"MammaMia: StreamingWatch found results for {showname}")
+        return url
+    except Exception as e:
+        print("MammaMia: StreamingWatch Failed", e)
+        return None
--- Src/Utilities/web.py
+++ Src/Utilities/web.py
@@ -11,7 +11,8 @@ chromium_path = "/usr/bin/chromium"
 
 
 class RequestManager:
-    def __init__(self):
+    def __init__(self, with_cookie: bool = True):
+        self.with_cookie = with_cookie
         self.cookies = None
         self.cookies_header = None
 
@@ -32,7 +33,7 @@ class RequestManager:
         await playwright.stop()
 
     async def get(self, api: str, more_headers: dict = None, get_json: bool = True):
-        if not self.cookies_header:
+        if (not self.cookies_header) and self.with_cookie:
             await self.__fetch_cookies()
 
         async with httpx.AsyncClient() as client:
@@ -44,14 +45,19 @@ class RequestManager:
                 "Accept": "application/json" if get_json else "text/html",
                 "Referer": base_url,
                 "Origin": base_url,
-                "Cookie": self.cookies_header
             }
+
+            if self.with_cookie:
+                headers.update({"Cookie": self.cookies_header, })
+
             if more_headers is not None:
                 headers.update(more_headers)
 
             response = await client.get(api, headers=headers)
-            if response.status_code == 401 or response.status_code == 403:
-                await self.__fetch_cookies()
-                return await self.get(api, more_headers, get_json)
+
+            if self.with_cookie:
+                if response.status_code == 401 or response.status_code == 403:
+                    await self.__fetch_cookies()
+                    return await self.get(api, more_headers, get_json)
 
             return response.json() if get_json else response.text
